9cd72bd604a10f1792718161d32949de25731e8a
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-01-05 22:01:01 -0600

    1/6 init

--- 010523.js
+++ 010523.js
@@ -21,17 +21,6 @@ console.log(breakingRecords([10,5,20,20,4,5,2,25,1]))
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 // Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there.
 
 // Example
