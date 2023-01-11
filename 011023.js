1cb4881c562fdae92588dbb8a75cf59003df085e
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-01-10 21:35:11 -0600

    1/10 solved

--- 011023.js
+++ 011023.js
@@ -1,8 +1,28 @@
 const sockMerchant = (n, ar) => {
-    
+    let newArray = ar
+    console.log("newarray", newArray)
+    let pairCount = 0
+    for (let i = n-1; i >= 0; i--){
+        console.log("first for loop step", i, "value at this index is", newArray[i])
+        for (let j=0;j<i; j++){
+            console.log("second for loop step", j, "value at this index is", newArray[j])
+            if (newArray[i] == newArray[j] && newArray[i]!=undefined){
+                newArray.splice(j, 1);
+                console.log("newarray after first splice", newArray)
+                newArray.splice(i-1, 1)
+                console.log("newarray after second splice", newArray)
+                pairCount ++
+                console.log(pairCount)
+            }
+        }
+    }
 
+    return pairCount
 }
 
+// console.log(sockMerchant(7, [1,2,1,2,1,3,2]))
+// console.log(sockMerchant(9, [10,20,20,10,10,30,50,10,20]))
+console.log(sockMerchant(10, [1, 1, 3, 1, 2, 1, 3, 3, 3, 3]))
 
 
 
