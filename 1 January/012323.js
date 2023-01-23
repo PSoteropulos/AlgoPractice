487077d89c265c7a0b167a1c46c6102af34e31a0
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-01-23 17:00:17 -0600

    01/23 init

---
+++ 1 January/012323.js
@@ -0,0 +1,32 @@
+const equalizeArray = (arr) => {
+    
+}
+
+
+
+
+
+
+
+
+// Given an array of integers, determine the minimum number of elements to delete to leave only elements of equal value.
+
+// Example
+
+
+// Delete the  elements  and  leaving . If both twos plus either the  or the  are deleted, it takes  deletions to leave either  or . The minimum number of deletions is .
+
+// Function Description
+
+// Complete the equalizeArray function in the editor below.
+
+// equalizeArray has the following parameter(s):
+
+// int arr[n]: an array of integers
+// Returns
+
+// int: the minimum number of deletions required
+// Input Format
+
+// The first line contains an integer , the number of elements in .
+// The next line contains  space-separated integers .
