fcacbb93f4e65b75e14bc655a694871b15a38ce3
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-01-30 19:18:54 -0600

    1/30 js solved

--- 1 January/013023.js
+++ 1 January/013023.js
@@ -21,8 +21,6 @@ console.log(alternatingCharacters("BABABA"))
 console.log(alternatingCharacters("AAABBB"))
 
 
-
-
 // You are given a string containing characters  and  only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
 
 // Your task is to find the minimum number of required deletions.
