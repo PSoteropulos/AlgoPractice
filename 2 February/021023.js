3481afdfa4cd6f27db54ee1dca40a967100233ff
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-02-10 00:08:25 -0600

    2/10 js and py solved

--- 2 February/021023.js
+++ 2 February/021023.js
@@ -1,10 +1,10 @@
 const sum = (num1, num2) => {
-    
+    return (num1 + num2)
 };
 
 
 
-
+console.log(sum(12, 5))
 
 
 
--- 2 February/021023.py
+++ 2 February/021023.py
@@ -1,10 +1,10 @@
 def sum(num1, num2):
-    
+    return num1+num2
 
 
 
 
-
+print(sum(12, 5))
 
 
 
