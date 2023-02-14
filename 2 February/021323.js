86d78ec4b404abce58cf46b1a7ee57b56cf1f728
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-02-13 18:00:05 -0600

    2/13 js solved

--- 2 February/021323.js
+++ 2 February/021323.js
@@ -1,7 +1,18 @@
+const numJewelsInStones = (jewels, stones) => {
+    let count = 0
+    for (let i=0; i<jewels.length; i++){
+        for (let j=0;j<stones.length;j++){
+            if (jewels[i] == stones[j]){
+                count ++
+            }
+        }
+    }
+    return count
+};
 
 
-
-
+console.log(numJewelsInStones("aA", "aAAbbbb"))
+console.log(numJewelsInStones("z", "ZZ"))
 
 
 
