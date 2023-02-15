76bbbeb54c9915d0fa2bc07e1a830cee1fc7d4b0
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-02-14 20:42:02 -0600

    2/14 py solved

--- 2 February/021423.py
+++ 2 February/021423.py
@@ -1,7 +1,15 @@
+def runningSum(nums):
+    sum = nums[0]
+    sumArray = [nums[0]]
+    for i in range(1, len(nums)):
+        sum += nums[i]
+        sumArray.append(sum)
+    return sumArray
 
 
-
-
+print(runningSum([1,2,3,4]))
+print(runningSum([1,1,1,1,1]))
+print(runningSum([3,1,2,10,1]))
 
 
 
