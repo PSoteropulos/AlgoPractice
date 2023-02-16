133280c5f29529e25a0e4404f031375b6d3fa750
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-02-15 21:01:16 -0600

    2/15 js solved

--- 2 February/021523.js
+++ 2 February/021523.js
@@ -14,9 +14,9 @@ const maximumWealth = (accounts) => {
 
 
 
-(maximumWealth([[1,2,3],[3,2,1]]))
-(maximumWealth([[1,5],[7,3],[3,5]]))
-(maximumWealth([[2,8,7],[7,1,3],[1,9,5]]))
+console.log(maximumWealth([[1,2,3],[3,2,1]]))
+console.log(maximumWealth([[1,5],[7,3],[3,5]]))
+console.log(maximumWealth([[2,8,7],[7,1,3],[1,9,5]]))
 
 
 
