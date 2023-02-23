ad60fe04f5fd897aa2232fb2af941ae1c6e00e79
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-02-22 19:40:07 -0600

    2/22 js solved

--- 2 February/022223.js
+++ 2 February/022223.js
@@ -1,9 +1,19 @@
 const differenceOfSum = (nums) => {
-    
+    let elementSum = 0
+    let digitSum = 0
+    for (let i=0; i<nums.length; i++){
+        elementSum += nums[i]
+        let numString = nums[i].toString()
+        for (let j=0; j<numString.length; j++){
+            digitSum += Number(numString[j])
+        }
+    }
+    return Math.abs(elementSum - digitSum)
 };
 
 
-
+console.log(differenceOfSum([1,15,6,3]))
+console.log(differenceOfSum([1,2,3,4]))
 
 
 
