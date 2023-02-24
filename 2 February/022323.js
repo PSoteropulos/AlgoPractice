cbd2746e1103744cc408f80e01cbcb1d9f20df9b
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-02-23 23:00:48 -0600

    2/23 js solved

--- 2 February/022323.js
+++ 2 February/022323.js
@@ -1,7 +1,15 @@
+var createTargetArray = function(nums, index) {
+    let targetArray = [ ]
+    for (let i=0; i<nums.length; i++){
+        targetArray.splice(index[i], 0, nums[i])
+    }
+    return targetArray
+};
 
 
-
-
+console.log(createTargetArray([0,1,2,3,4], [0,1,2,2,1]))
+console.log(createTargetArray([1,2,3,4,0], [0,1,2,3,0]))
+console.log(createTargetArray([1], [0]))
 
 
 
