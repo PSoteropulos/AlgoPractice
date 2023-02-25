e9d9248c50b9e8ad97d4ba307a209ca6747d5c7e
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-02-24 23:47:29 -0600

    2/24 js solved

--- 2 February/022423.js
+++ 2 February/022423.js
@@ -1,10 +1,17 @@
 const decompressRLElist = (nums) => {
-    
+    let finalArray = [ ]
+    for (let i=0; i<nums.length; i += 2){
+        for (let j=0; j<nums[i];j++){
+            finalArray.push(nums[i+1])
+        }
+    }
+    return finalArray
 };
 
 
 
-
+console.log(decompressRLElist([1,2,3,4]))
+console.log(decompressRLElist([1,1,2,3]))
 
 
 
