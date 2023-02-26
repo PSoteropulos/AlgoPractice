b3883ca6c4a3cb168ec43272816898671f2904c1
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-02-25 19:00:40 -0600

    2/25 js solved

--- 2 February/022523.js
+++ 2 February/022523.js
@@ -1,9 +1,14 @@
 const restoreString = (s, indices) => {
-    
+    let finalString = ''
+    for (let i=0; i<s.length;i++){
+        finalString = finalString.concat(s[indices.indexOf(i)])
+    }
+    return finalString
 };
 
 
-
+console.log(restoreString("codeleet", [4,5,6,7,0,2,1,3]))
+console.log(restoreString("abc", [0,1,2]))
 
 
 
