2a627db1aeb186ca0b0adc23224a2dc0854169c0
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-03-04 16:08:08 -0600

    3/4 init

---
+++ 3 March/030423.py
@@ -0,0 +1,57 @@
+def arithmeticTriplets(nums, diff):
+    totalTriplets = 0
+    for i in range(len(nums)-1):
+        possibleTriplets = 0
+        secondIndex = 0
+        for j in range(i+1, len(nums)):
+            if nums[i] + diff == nums[j]:
+                possibleTriplets += 1
+                secondIndex = j
+        for k in range(secondIndex, len(nums)):
+            if nums[secondIndex] + diff == nums[k]:
+                totalTriplets += 1
+    return totalTriplets
+
+
+
+
+
+
+
+
+# 2367. Number of Arithmetic Triplets
+# Easy
+# 774
+# 33
+# Companies
+# You are given a 0-indexed, strictly increasing integer array nums and a positive integer diff. A triplet (i, j, k) is an arithmetic triplet if the following conditions are met:
+
+# i < j < k,
+# nums[j] - nums[i] == diff, and
+# nums[k] - nums[j] == diff.
+# Return the number of unique arithmetic triplets.
+
+ 
+
+# Example 1:
+
+# Input: nums = [0,1,4,6,7,10], diff = 3
+# Output: 2
+# Explanation:
+# (1, 2, 4) is an arithmetic triplet because both 7 - 4 == 3 and 4 - 1 == 3.
+# (2, 4, 5) is an arithmetic triplet because both 10 - 7 == 3 and 7 - 4 == 3. 
+# Example 2:
+
+# Input: nums = [4,5,6,7,8,9], diff = 2
+# Output: 2
+# Explanation:
+# (0, 2, 4) is an arithmetic triplet because both 8 - 6 == 2 and 6 - 4 == 2.
+# (1, 3, 5) is an arithmetic triplet because both 9 - 7 == 2 and 7 - 5 == 2.
+ 
+
+# Constraints:
+
+# 3 <= nums.length <= 200
+# 0 <= nums[i] <= 200
+# 1 <= diff <= 50
+# nums is strictly increasing.
