a7bd9765c56470bd8c69ff91898fda547c3c81cc
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-03-10 23:05:25 -0600

    3/10 js solved

--- 3 March/031023.js
+++ 3 March/031023.js
@@ -1,4 +1,14 @@
-
+const countKDifference = (nums, k) => {
+    let count = 0
+    for (let i=0; i<nums.length-1; i++){
+        for (let j=i+1; j<nums.length; j++){
+            if (Math.abs(nums[i]-nums[j])==k){
+                count += 1
+            }
+        }
+    }
+    return count
+};
 
 
 
