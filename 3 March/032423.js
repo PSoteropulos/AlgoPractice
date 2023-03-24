b36ed2224666d363568c599fbe7e1b337b8ebc94
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-03-24 17:55:11 -0500

    3/24 js solved

--- 3 March/032423.js
+++ 3 March/032423.js
@@ -1,5 +1,17 @@
 const replaceDigits = (s) => {
-    
+    let alphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let newString = ""
+    for (let i=0; i<s.length; i++){
+        if ((i == 0) || (i%2==0)){
+            newString += s[i]
+        }
+        else {
+            let digit = Number(s[i])
+            let referencePoint = alphabetArray.indexOf(`${s[i-1]}`)
+            newString += String(alphabetArray[referencePoint+digit])
+        }
+    }
+    return newString
 };
 
 
