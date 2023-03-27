900f1af72dd8e49c741cc5d017a66945dd268715
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-03-26 20:58:29 -0500

    3/26 js solved

--- 3 March/032623.js
+++ 3 March/032623.js
@@ -1,6 +1,12 @@
 const numOfStrings = (patterns, word) => {
-    
-};
+    let count = 0
+    for (let i=0; i<patterns.length; i++){
+        if (word.includes(patterns[i])){
+            count ++
+        }
+    }
+    return count
+}
 
 
 
