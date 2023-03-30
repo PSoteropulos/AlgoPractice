f0b49e4f4994a3947d61e359d8e9face67a9666f
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-03-30 16:49:06 -0500

    3/30 js solved

--- 3 March/033023.js
+++ 3 March/033023.js
@@ -1,6 +1,13 @@
 const countPairs = (nums, k) => {
     let count = 0
-    for (let i)
+    for (let i=0; i<nums.length-1; i++){
+        for (let j=i+1;j<nums.length;j++){
+            if ((nums[i]===nums[j]) && ((i*j)%k===0)){
+                count ++
+            }
+        }
+    }
+    return count
 };
 
 
