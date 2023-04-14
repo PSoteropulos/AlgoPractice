75ac6510dff2398b7f810baeeec45ba0f4957330
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-04-13 23:08:39 -0500

    4/13 init

---
+++ 4 April/041323.js
@@ -0,0 +1,51 @@
+const prefixCount = (words, pref) => {
+    let counts = 0
+    for (let i=0; i<words.length; i++){
+        
+    }
+};
+
+
+
+console.log(prefixCount(["pay","attention","practice","attend"], "at"))
+console.log(prefixCount(["leetcode","win","loops","success"], "code"))
+
+
+
+
+// 2185. Counting Words With a Given Prefix
+// Easy
+// 525
+// 14
+// Companies
+// You are given an array of strings words and a string pref.
+
+// Return the number of strings in words that contain pref as a prefix.
+
+// A prefix of a string s is any leading contiguous substring of s.
+
+ 
+
+// Example 1:
+
+// Input: words = ["pay","attention","practice","attend"], pref = "at"
+// Output: 2
+// Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+// Example 2:
+
+// Input: words = ["leetcode","win","loops","success"], pref = "code"
+// Output: 0
+// Explanation: There are no strings that contain "code" as a prefix.
+ 
+
+// Constraints:
+
+// 1 <= words.length <= 100
+// 1 <= words[i].length, pref.length <= 100
+// words[i] and pref consist of lowercase English letters.
+// Accepted
+// 62K
+// Submissions
+// 80.2K
+// Acceptance Rate
+// 77.3%
