c16efc25eead3c3a528b0e65d68c251c37229ba0
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-04-17 14:19:29 -0500

    4/17 js solved

--- 4 April/041723.js
+++ 4 April/041723.js
@@ -1,5 +1,11 @@
 const findNumbers = (nums) => {
-    
+    let count = 0
+    for (let i=0; i<nums.length; i++){
+        if (nums[i].toString().length % 2 === 0){
+            count ++
+        }
+    }
+    return count
 };
 
 
