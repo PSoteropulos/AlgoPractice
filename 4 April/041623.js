d43bfd77260558b7e8e943a337071a84e865f5ea
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-04-16 22:31:05 -0500

    4/16 scrap, new prob init

--- 4 April/041623.js
+++ 4 April/041623.js
@@ -1,54 +1,58 @@
-const minSteps = (s, t) => {
+const findGCD = (nums) => {
     
 };
 
 
-
-console.log(minSteps("bab", "aba"))
-console.log(minSteps("leetcode", "practice"))
-console.log(minSteps("anagram", "mangaar"))
-
+console.log(findGCD([2,5,6,9,10]))
+console.log(findGCD([7,5,6,8,3]))
+console.log(findGCD([3,3]))
 
 
 
-// 1347. Minimum Number of Steps to Make Two Strings Anagram
-// Medium
-// 1.8K
-// 77
+// 1979. Find Greatest Common Divisor of Array
+// Easy
+// 840
+// 33
 // Companies
-// You are given two strings of the same length s and t. In one step you can choose any character of t and replace it with another character.
+// Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
 
-// Return the minimum number of steps to make t an anagram of s.
-
-// An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+// The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
 
  
 
 // Example 1:
 
-// Input: s = "bab", t = "aba"
-// Output: 1
-// Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s.
+// Input: nums = [2,5,6,9,10]
+// Output: 2
+// Explanation:
+// The smallest number in nums is 2.
+// The largest number in nums is 10.
+// The greatest common divisor of 2 and 10 is 2.
 // Example 2:
 
-// Input: s = "leetcode", t = "practice"
-// Output: 5
-// Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
+// Input: nums = [7,5,6,8,3]
+// Output: 1
+// Explanation:
+// The smallest number in nums is 3.
+// The largest number in nums is 8.
+// The greatest common divisor of 3 and 8 is 1.
 // Example 3:
 
-// Input: s = "anagram", t = "mangaar"
-// Output: 0
-// Explanation: "anagram" and "mangaar" are anagrams. 
+// Input: nums = [3,3]
+// Output: 3
+// Explanation:
+// The smallest number in nums is 3.
+// The largest number in nums is 3.
+// The greatest common divisor of 3 and 3 is 3.
  
 
 // Constraints:
 
-// 1 <= s.length <= 5 * 104
-// s.length == t.length
-// s and t consist of lowercase English letters only.
+// 2 <= nums.length <= 1000
+// 1 <= nums[i] <= 1000
 // Accepted
-// 127.3K
+// 81.3K
 // Submissions
-// 163.5K
+// 105.6K
 // Acceptance Rate
-// 77.8%
+// 77.0%
