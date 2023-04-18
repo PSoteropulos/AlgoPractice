f4a15b78cf158ed335904616a7908a198ea76427
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-04-18 14:12:36 -0500

    4/18 init

---
+++ 4 April/041823.py
@@ -0,0 +1,45 @@
+def sumZero(n):
+    pass
+
+
+
+
+print(sumZero(5))
+print(sumZero(3))
+print(sumZero(1))
+
+
+
+# 1304. Find N Unique Integers Sum up to Zero
+# Easy
+# 1.7K
+# 548
+# Companies
+# Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+ 
+
+# Example 1:
+
+# Input: n = 5
+# Output: [-7,-1,1,3,4]
+# Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+# Example 2:
+
+# Input: n = 3
+# Output: [-1,0,1]
+# Example 3:
+
+# Input: n = 1
+# Output: [0]
+ 
+
+# Constraints:
+
+# 1 <= n <= 1000
+# Accepted
+# 183.2K
+# Submissions
+# 238.2K
+# Acceptance Rate
+# 76.9%
