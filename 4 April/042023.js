32a4082da2013bfd518cadb2341abc275067f152
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-04-20 23:04:50 -0500

    4/20 init and js solved

---
+++ 4 April/042023.js
@@ -0,0 +1,42 @@
+const reverseString = function(s) {
+    s.reverse()
+};
+
+
+
+
+
+
+
+
+// 344. Reverse String
+// Easy
+// 7.2K
+// 1.1K
+// Companies
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
+// Accepted
+// 2.1M
+// Submissions
+// 2.7M
+// Acceptance Rate
+// 76.8%
