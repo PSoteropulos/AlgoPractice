10efb23233cc6e87edfbc0dce58a0bc6f134afe4
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-04-21 20:06:45 -0500

    4/21 init

---
+++ 4 April/042123.js
@@ -0,0 +1,51 @@
+const sumOfUnique = (nums) => {
+    
+};
+
+
+
+console.log(sumOfUnique([1,2,3,2]))
+console.log(sumOfUnique([1,1,1,1,1]))
+console.log(sumOfUnique([1,2,3,4,5]))
+
+
+
+
+// 1748. Sum of Unique Elements
+// Easy
+// 1.2K
+// 28
+// Companies
+// You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+// Return the sum of all the unique elements of nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,2]
+// Output: 4
+// Explanation: The unique elements are [1,3], and the sum is 4.
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: 0
+// Explanation: There are no unique elements, and the sum is 0.
+// Example 3:
+
+// Input: nums = [1,2,3,4,5]
+// Output: 15
+// Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+// Accepted
+// 115.5K
+// Submissions
+// 151.3K
+// Acceptance Rate
+// 76.3%
