d7c6fd59ca8455d878f3751b562b5d0ceb24fafc
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-04-24 21:14:52 -0500

    4/24 js solved

--- 4 April/042423.js
+++ 4 April/042423.js
@@ -1,5 +1,11 @@
 const repeatedNTimes = (nums) => {
-    
+    for (let i=0; i<nums.length-1;i++){
+        for (let j=i+1;j<nums.length;j++){
+            if (nums[i] === nums[j]){
+                return nums[i]
+            }
+        }
+    }
 };
 
 
