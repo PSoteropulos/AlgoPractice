9bd6fbf1e9e64fb98c1d7961054d83b0a9e820b5
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-04-26 22:44:35 -0500

    4/26 js solved

--- 4 April/042623.js
+++ 4 April/042623.js
@@ -1,8 +1,11 @@
 const busyStudent = (startTime, endTime, queryTime) => {
     let count = 0
     for (let i = 0; i<startTime.length; i++){
-        
+        if ((startTime[i] <= queryTime) && (queryTime <= endTime[i])){
+            count ++
+        }
     }
+    return count
 };
 
 
