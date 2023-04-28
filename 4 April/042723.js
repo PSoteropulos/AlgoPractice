4e6c933cabadada7e0593c76c0f9fa3741983d8b
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-04-27 23:34:05 -0500

    4/27 js solved

--- 4 April/042723.js
+++ 4 April/042723.js
@@ -1,5 +1,13 @@
 const heightChecker = (heights) => {
-    let 
+    let heightsCopy = [...heights]
+    heights.sort((a, b)=>(a-b))
+    let count = 0
+    for (let i=0; i<heights.length;i++){
+        if (heightsCopy[i] !== heights[i]){
+            count ++
+        }
+    }
+    return count
 };
 
 
