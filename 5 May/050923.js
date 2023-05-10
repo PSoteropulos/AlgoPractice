a500c02d8843ee30ff27fec3ddb1d18307ee698d
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-05-09 23:34:44 -0500

    5/9 js solved, ugly ugly

--- 5 May/050923.js
+++ 5 May/050923.js
@@ -1,14 +1,46 @@
 const twoOutOfThree = (nums1, nums2, nums3) => {
-    
+    nums1.sort((a,b)=>(a-b))
+    nums2.sort((a,b)=>(a-b))
+    nums3.sort((a,b)=>(a-b))
+    console.log({num1:nums1, num2:nums2, num3:nums3})
+    let obj = {}
+    let answerArray = []
+    for (let i = 0; i<nums1.length;i++){
+        if ((`${nums1[i]}` in obj) === false){
+            obj[nums1[i]] = 1
+        }
+    }
+    for (let j = 0; j<nums2.length;j++){
+        if ((`${nums2[j]}` in obj) === false){
+            obj[nums2[j]] = 1
+        }
+        else if (((`${nums2[j]}` in obj) === true) && (nums2[j] !== nums2[j-1])){
+            obj[nums2[j]] ++
+        }
+    }
+    for (let h = 0; h<nums3.length;h++){
+        if ((`${nums3[h]}` in obj) === false){
+            obj[nums3[h]] = 1
+        }
+        else if (((`${nums3[h]}` in obj) === true) && (nums3[h] !== nums3[h-1])){
+            obj[nums3[h]] ++
+        }
+    }
+    for (const [key, value] of Object.entries(obj)){
+        if (value > 1){
+            answerArray.push(key)
+        }
+    }
+    return answerArray
 };
 
 
 
 
-console.log(twoOutOfThree([1,1,3,2], [2,3], [3]))
-console.log(twoOutOfThree([3,1], [2,3], [1,2]))
-console.log(twoOutOfThree([1,2,2], [4,3,3], [5]))
-
+// console.log(twoOutOfThree([1,1,3,2], [2,3], [3]))
+// console.log(twoOutOfThree([3,1], [2,3], [1,2]))
+// console.log(twoOutOfThree([1,2,2], [4,3,3], [5]))
+console.log(twoOutOfThree([2,15,10,11,14,12,14,11,9,1], [8,9,13,2,11,8], [13,5,15,7,12,7,8,3,13,15]))
 
 
 // 2032. Two Out of Three
