b088646aee9cfc39b7bcfc9e57727217ceff7c7e
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-05-27 03:23:00 -0500

    5/27 init

---
+++ 5 May/052723.py
@@ -0,0 +1,53 @@
+def sortArrayByParityII(nums):
+    pass
+
+
+
+
+print(sortArrayByParityII([4,2,5,7]))
+print(sortArrayByParityII([2,3]))
+
+
+
+
+
+# 922. Sort Array By Parity II
+# Easy
+# 2.3K
+# 84
+# Companies
+# Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+
+# Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+
+# Return any answer array that satisfies this condition.
+
+ 
+
+# Example 1:
+
+# Input: nums = [4,2,5,7]
+# Output: [4,5,2,7]
+# Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+# Example 2:
+
+# Input: nums = [2,3]
+# Output: [2,3]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 2 * 104
+# nums.length is even.
+# Half of the integers in nums are even.
+# 0 <= nums[i] <= 1000
+ 
+
+# Follow Up: Could you solve it in-place?
+
+# Accepted
+# 219.8K
+# Submissions
+# 311.3K
+# Acceptance Rate
+# 70.6%
