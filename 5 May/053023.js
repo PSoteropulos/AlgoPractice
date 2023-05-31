0b78814e2f23c35770f36242e49475539aa039bf
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-05-30 22:23:52 -0500

    5/30 js solved

--- 5 May/053023.js
+++ 5 May/053023.js
@@ -1,8 +1,6 @@
 var fib = function(n) {
-    if (n === 0){
-        return 0
-    }
-};
+    return n===0?0:n===1?1:fib(n-1)+fib(n-2)
+}
 
 
 
@@ -10,8 +8,8 @@ var fib = function(n) {
 console.log(fib(2))
 console.log(fib(3))
 console.log(fib(4))
-
-
+console.log(fib(5))
+console.log(fib(6))
 
 
 // 509. Fibonacci Number
