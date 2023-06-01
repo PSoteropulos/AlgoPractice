22df3b2e82802684f8f8b0225963ca293f158399
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-05-31 21:53:30 -0500

    5/31 init

---
+++ 5 May/053123.py
@@ -0,0 +1,58 @@
+def countWords(words1, words2):
+    pass
+
+
+
+
+
+print(countWords(["leetcode","is","amazing","as","is"], ["amazing","leetcode","is"]))
+print(countWords(["b","bb","bbb"], ["a","aa","aaa"]))
+print(countWords(["a","ab"], ["a","a","a","ab"]))
+
+
+
+
+
+
+# 2085. Count Common Words With One Occurrence
+# Easy
+# 653
+# 14
+# Companies
+# Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.
+
+ 
+
+# Example 1:
+
+# Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+# Output: 2
+# Explanation:
+# - "leetcode" appears exactly once in each of the two arrays. We count this string.
+# - "amazing" appears exactly once in each of the two arrays. We count this string.
+# - "is" appears in each of the two arrays, but there are 2 occurrences of it in words1. We do not count this string.
+# - "as" appears once in words1, but does not appear in words2. We do not count this string.
+# Thus, there are 2 strings that appear exactly once in each of the two arrays.
+# Example 2:
+
+# Input: words1 = ["b","bb","bbb"], words2 = ["a","aa","aaa"]
+# Output: 0
+# Explanation: There are no strings that appear in each of the two arrays.
+# Example 3:
+
+# Input: words1 = ["a","ab"], words2 = ["a","a","a","ab"]
+# Output: 1
+# Explanation: The only string that appears exactly once in each of the two arrays is "ab".
+ 
+
+# Constraints:
+
+# 1 <= words1.length, words2.length <= 1000
+# 1 <= words1[i].length, words2[j].length <= 30
+# words1[i] and words2[j] consists only of lowercase English letters.
+# Accepted
+# 46.9K
+# Submissions
+# 67K
+# Acceptance Rate
+# 69.9%
