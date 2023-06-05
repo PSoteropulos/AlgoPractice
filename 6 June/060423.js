f9dba64154f324f23f24cd47523c75deb1130f83
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-06-04 23:04:38 -0500

    6/4 js solved

--- 6 June/060423.js
+++ 6 June/060423.js
@@ -1,5 +1,18 @@
 const minimumAbsDifference = (arr) => {
-    
+    arr.sort((a,b) => a-b)
+    let minDiff = Infinity
+    let ansArray = []
+    for (let i = 0; i<arr.length-1; i++){
+        if (arr[i+1] - arr[i] < minDiff){
+            minDiff = arr[i+1] - arr[i]
+        }
+    }
+    for (let j=0; j<arr.length-1; j++){
+        if (arr[j+1] - arr[j] === minDiff){
+            ansArray.push([arr[j], arr[j+1]])
+        }
+    }
+    return ansArray
 };
 
 
