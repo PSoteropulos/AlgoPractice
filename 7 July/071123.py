ca7a21aca44a94d06a721c0d9fe3539c8618bd66
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-07-11 21:30:29 -0500

    7/11 py solved

--- 7 July/071123.py
+++ 7 July/071123.py
@@ -1,5 +1,8 @@
 def threeConsecutiveOdds(arr):
-    pass
+    for i in range(len(arr)-2):
+        if (arr[i]%2!=0 and arr[i+1]%2!=0 and arr[i+2]%2!=0):
+            return True
+    return False
 
 
 
