134186928d6f06907cc3a58941576700e9daaa46
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-07-15 22:34:12 -0500

    7/15 js solved

--- 7 July/071523.js
+++ 7 July/071523.js
@@ -1,5 +1,11 @@
 const moveZeroes = (nums) => {
-    
+    for (let i = nums.length-1; i >= 0; i--){
+        if (nums[i]===0){
+            nums.splice(i,1)
+            nums.push(0)
+        }
+    }
+    return nums
 };
 
 
