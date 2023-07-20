853cf57f2cc5bf8f469678094c211c0352cddd80
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-07-19 22:15:59 -0500

    7/19 js solved

--- 7 July/071923.js
+++ 7 July/071923.js
@@ -1,5 +1,23 @@
 const reverseOnlyLetters = (s) => {
-    
+    const alphabetArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    let tempArray = []
+    let answerString = ""
+    for (let i = 0; i < s.length; i++){
+        if (alphabetArray.includes(s[i])){
+            tempArray.push(s[i])
+        }
+    }
+    tempArray.reverse()
+    for (let j=0; j<s.length;j++){
+        if (alphabetArray.includes(s[j])){
+            answerString += tempArray[0]
+            tempArray.splice(0,1)
+        }
+        else {
+            answerString += s[j]
+        }
+    }
+    return answerString
 };
 
 
