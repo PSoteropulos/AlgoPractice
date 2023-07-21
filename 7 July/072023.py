32f0edc3be3ec5786834b433ae90d5cef9ca148e
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-07-20 22:11:12 -0500

    7/20 init

---
+++ 7 July/072023.py
@@ -0,0 +1,48 @@
+def maxPower(s):
+    pass
+
+
+
+
+
+print(maxPower("leetcode"))
+print(maxPower("abbcccddddeeeeedcba"))
+
+
+
+
+
+
+# 1446. Consecutive Characters
+# Easy
+# 1.6K
+# 30
+# Companies
+# The power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+# Given a string s, return the power of s.
+
+ 
+
+# Example 1:
+
+# Input: s = "leetcode"
+# Output: 2
+# Explanation: The substring "ee" is of length 2 with the character 'e' only.
+# Example 2:
+
+# Input: s = "abbcccddddeeeeedcba"
+# Output: 5
+# Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 500
+# s consists of only lowercase English letters.
+# Accepted
+# 144.1K
+# Submissions
+# 237.2K
+# Acceptance Rate
+# 60.8%
