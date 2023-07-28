4b172b1892bd4d50be6356b463bdcef67a837a9b
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-07-27 22:35:20 -0500

    7/27 init

---
+++ 7 July/072723.js
@@ -0,0 +1,51 @@
+const findDisappearedNumbers = (nums) => {
+    
+};
+
+
+
+
+
+
+console.log(findDisappearedNumbers([4,3,2,7,8,2,3,1]))
+console.log(findDisappearedNumbers([1,1]))
+
+
+
+
+
+
+// 448. Find All Numbers Disappeared in an Array
+// Easy
+// 8.7K
+// 441
+// Companies
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [5,6]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [2]
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 105
+// 1 <= nums[i] <= n
+ 
+
+// Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+// Accepted
+// 755.6K
+// Submissions
+// 1.3M
+// Acceptance Rate
+// 60.2%
