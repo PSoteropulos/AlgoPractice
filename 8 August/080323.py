dd6e9deec34746d457b11aea2c0395e637dd3492
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-08-03 22:38:43 -0500

    8/3 init

---
+++ 8 August/080323.py
@@ -0,0 +1,50 @@
+def isAnagram(s, t):
+    pass
+
+
+
+
+
+print(isAnagram("anagram", "nagaram"))
+print(isAnagram("rat", "car"))
+
+
+
+
+
+
+# 242. Valid Anagram
+# Easy
+# 10K
+# 319
+# Companies
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+# Example 2:
+
+# Input: s = "rat", t = "car"
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
+ 
+
+# Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+# Accepted
+# 2.4M
+# Submissions
+# 3.8M
+# Acceptance Rate
+# 63.1%
