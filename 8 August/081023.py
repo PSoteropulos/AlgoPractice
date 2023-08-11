51f1e257da28e057f7935fc233d8a69de39b5dee
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-08-10 23:00:07 -0500

    8/10 init

---
+++ 8 August/081023.py
@@ -0,0 +1,52 @@
+def canConstruct(ransomNote, magazine):
+    pass
+
+
+
+
+
+
+print(canConstruct("a", magazine = "b"))
+print(canConstruct("aa", magazine = "ab"))
+print(canConstruct("aa", magazine = "aab"))
+
+
+
+
+
+
+# 383. Ransom Note
+# Easy
+# 4.3K
+# 443
+# Companies
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+# Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+# Example 1:
+
+# Input: ransomNote = "a", magazine = "b"
+# Output: false
+# Example 2:
+
+# Input: ransomNote = "aa", magazine = "ab"
+# Output: false
+# Example 3:
+
+# Input: ransomNote = "aa", magazine = "aab"
+# Output: true
+ 
+
+# Constraints:
+
+# 1 <= ransomNote.length, magazine.length <= 105
+# ransomNote and magazine consist of lowercase English letters.
+# Accepted
+# 838.5K
+# Submissions
+# 1.4M
+# Acceptance Rate
+# 58.9%
