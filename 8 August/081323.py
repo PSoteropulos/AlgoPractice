351feabad626ebf715db5de9f1fea1a91ca831c8
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-08-13 22:23:05 -0500

    8/13 init

---
+++ 8 August/081323.py
@@ -0,0 +1,53 @@
+def isMonotonic(nums):
+    pass
+
+
+
+
+
+print(isMonotonic([1,2,2,3]))
+print(isMonotonic([6,5,4,4]))
+print(isMonotonic([1,3,2]))
+
+
+
+
+
+
+# 896. Monotonic Array
+# Easy
+# 2.2K
+# 64
+# Companies
+# An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+# An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+# Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,2,3]
+# Output: true
+# Example 2:
+
+# Input: nums = [6,5,4,4]
+# Output: true
+# Example 3:
+
+# Input: nums = [1,3,2]
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -105 <= nums[i] <= 105
+# Accepted
+# 274.3K
+# Submissions
+# 468.6K
+# Acceptance Rate
+# 58.5%
