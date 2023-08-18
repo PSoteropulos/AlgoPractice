bc904edfc0d687fcfd5768ad65e7d0f1191bcdc4
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-08-18 18:49:58 -0500

    8/16 re init

--- 8 August/081623.js
+++ 8 August/081623.js
@@ -1,64 +1,55 @@
-const maxSum = (nums) => {
-    nums.sort((a,b)=>b-a)
-    let answers = []
-    for (let i = 0; i<nums.length-1; i++){
-        for (let h=9;h>=0;h--){
-            for (let j=i+1; j<nums.length; j++){
-                if (nums[i].toString().includes(h) && nums[j].toString().includes(h)){
-                    console.log(nums[i], nums[j])
-                    return(nums[i]+nums[j])
-                }
-            }
-        }
-    }
-    return -1
+const sumOfMultiples = (n) => {
+    
 };
 
 
 
 
 
-console.log(maxSum([51,71,17,24,42]))
-console.log(maxSum([1,2,3,4]))
-console.log(maxSum([31,25,72,79,74]))
+console.log(sumOfMultiples(7))
+console.log(sumOfMultiples(10))
+console.log(sumOfMultiples(9))
 
 
 
 
 
-// 2815. Max Pair Sum in an Array
+
+
+// 2652. Sum Multiples
 // Easy
-// 151
-// 58
+// 372
+// 24
 // Companies
-// You are given a 0-indexed integer array nums. You have to find the maximum sum of a pair of numbers from nums such that the maximum digit in both numbers are equal.
+// Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
 
-// Return the maximum sum or -1 if no such pair exists.
+// Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
 
  
 
 // Example 1:
 
-// Input: nums = [51,71,17,24,42]
-// Output: 88
-// Explanation: 
-// For i = 1 and j = 2, nums[i] and nums[j] have equal maximum digits with a pair sum of 71 + 17 = 88. 
-// For i = 3 and j = 4, nums[i] and nums[j] have equal maximum digits with a pair sum of 24 + 42 = 66.
-// It can be shown that there are no other pairs with equal maximum digits, so the answer is 88.
+// Input: n = 7
+// Output: 21
+// Explanation: Numbers in the range [1, 7] that are divisible by 3, 5, or 7 are 3, 5, 6, 7. The sum of these numbers is 21.
 // Example 2:
 
-// Input: nums = [1,2,3,4]
-// Output: -1
-// Explanation: No pair exists in nums with equal maximum digits.
+// Input: n = 10
+// Output: 40
+// Explanation: Numbers in the range [1, 10] that are divisible by 3, 5, or 7 are 3, 5, 6, 7, 9, 10. The sum of these numbers is 40.
+// Example 3:
+
+// Input: n = 9
+// Output: 30
+// Explanation: Numbers in the range [1, 9] that are divisible by 3, 5, or 7 are 3, 5, 6, 7, 9. The sum of these numbers is 30.
  
 
 // Constraints:
 
-// 2 <= nums.length <= 100
-// 1 <= nums[i] <= 104
+// 1 <= n <= 103
 // Accepted
-// 26K
+// 61.9K
 // Submissions
-// 44.8K
+// 72.8K
 // Acceptance Rate
-// 58.0%
+// 85.1%
