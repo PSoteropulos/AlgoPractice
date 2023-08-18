d96d4be3be094ed508bcbb17f2d6eb6698dbb277
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-08-18 18:40:06 -0500

    8/17 js solved

--- 8 August/081723.js
+++ 8 August/081723.js
@@ -1,5 +1,11 @@
 const numberOfEmployeesWhoMetTarget = (hours, target) => {
-    
+    let count = 0
+    for (let i=0;i<hours.length;i++){
+        if (hours[i] >= target){
+            count ++
+        }
+    }
+    return count
 };
 
 
