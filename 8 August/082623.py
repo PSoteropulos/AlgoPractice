396283dac54ceca9767708c9c13734c034403c21
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-08-26 20:39:11 -0500

    8/26 init

---
+++ 8 August/082623.py
@@ -0,0 +1,46 @@
+def findMaxConsecutiveOnes(nums):
+    pass
+
+
+
+
+
+print(findMaxConsecutiveOnes([1,1,0,1,1,1]))
+print(findMaxConsecutiveOnes([1,0,1,1,0,1]))
+
+
+
+
+
+
+
+# 485. Max Consecutive Ones
+# Easy
+# 5K
+# 442
+# Companies
+# Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,0,1,1,1]
+# Output: 3
+# Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+# Example 2:
+
+# Input: nums = [1,0,1,1,0,1]
+# Output: 2
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# nums[i] is either 0 or 1.
+# Accepted
+# 945.7K
+# Submissions
+# 1.6M
+# Acceptance Rate
+# 57.5%
