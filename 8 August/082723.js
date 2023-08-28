1308940c74d5621efa98da0f81f1020c56db0caa
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-08-27 22:26:29 -0500

    8/27 init

---
+++ 8 August/082723.js
@@ -0,0 +1,52 @@
+const kLengthApart = (nums, k) => {
+    
+};
+
+
+
+
+
+
+console.log(kLengthApart(nums = [1,0,0,0,1,0,0,1], k = 2))
+console.log(kLengthApart(nums = [1,0,0,1,0,1], k = 2))
+
+
+
+
+
+
+
+// 1437. Check If All 1's Are at Least Length K Places Away
+// Easy
+// 587
+// 218
+// Companies
+// Given an binary array nums and an integer k, return true if all 1's are at least k places away from each other, otherwise return false.
+
+ 
+
+// Example 1:
+
+
+// Input: nums = [1,0,0,0,1,0,0,1], k = 2
+// Output: true
+// Explanation: Each of the 1s are at least 2 places away from each other.
+// Example 2:
+
+
+// Input: nums = [1,0,0,1,0,1], k = 2
+// Output: false
+// Explanation: The second 1 and third 1 are only one apart from each other.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 0 <= k <= nums.length
+// nums[i] is 0 or 1
+// Accepted
+// 69.8K
+// Submissions
+// 119.6K
+// Acceptance Rate
+// 58.3%
