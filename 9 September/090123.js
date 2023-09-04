f68fa576c0f40714f06d171e0dfb887af8cced29
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-09-03 20:31:30 -0500

    9/1 js solved

--- 9 September/090123.js
+++ 9 September/090123.js
@@ -1,5 +1,13 @@
 const maximumNumberOfStringPairs = (words) => {
-    
+    let count = 0
+    for (let i=0; i<words.length-1;i++){
+        for (let j=i+1;j<words.length;j++){
+            if (words[i] === words[j].split("").reverse().join("")){
+                count ++
+            }
+        }
+    }
+    return count
 };
 
 
