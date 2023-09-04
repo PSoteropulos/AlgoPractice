25ae3e1d52b6532cd56d32a507507ec587c810aa
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-09-03 20:57:50 -0500

    9/33 js solved

--- 9 September/090323.js
+++ 9 September/090323.js
@@ -1,5 +1,11 @@
 const sumOfSquares = (nums) => {
-    
+    let squares = 0
+    for (let i=1; i<=nums.length;i++){
+        if (nums.length % i === 0){
+            squares += nums[i-1] * nums[i-1]
+        }
+    }
+    return squares
 };
 
 
