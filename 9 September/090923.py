c19c240c58a084cae31b40b3ee353e766d721aff
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-09-10 23:35:34 -0500

    9/9 py solved

--- 9 September/090923.py
+++ 9 September/090923.py
@@ -1,5 +1,13 @@
 def findPoisonedDuration(timeSeries, duration):
-    pass
+    count = 0
+    for i in range(len(timeSeries)):
+        if (i == len(timeSeries)-1):
+            count += duration
+        elif (timeSeries[i] + duration >= timeSeries[i+1]):
+            count += (timeSeries[i+1] - timeSeries[i])
+        else:
+            count += duration
+    return count
 
 
 
