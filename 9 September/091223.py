734b3db9b77f653c7068b84b013d42d9d62d8313
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-09-12 20:35:07 -0500

    9/12 py solved

--- 9 September/091223.py
+++ 9 September/091223.py
@@ -1,5 +1,10 @@
 def maximumDifference(nums):
-    pass
+    maxDiff = -1
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if (nums[i]<nums[j] and nums[j]-nums[i]>maxDiff):
+                maxDiff = nums[j]-nums[i]
+    return maxDiff
 
 
 
