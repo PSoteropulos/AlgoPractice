cb69dc2db65acadbbb7e1c119a048b63aacfdd09
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-09-19 00:09:10 -0500

    9/19 js solved

--- 9 September/091923.js
+++ 9 September/091923.js
@@ -1,5 +1,17 @@
 const findDifference = (nums1, nums2) => {
-    
+    let set1 = new Set()
+    let set2 = new Set()
+    for (let i=0;i<nums1.length;i++){
+        if (!nums2.includes(nums1[i])){
+            set1.add(nums1[i])
+        }
+    }
+    for (let i=0;i<nums2.length;i++){
+        if (!nums1.includes(nums2[i])){
+            set2.add(nums2[i])
+        }
+    }
+    return [Array.from(set1), Array.from(set2)]
 };
 
 
@@ -9,8 +21,7 @@ const findDifference = (nums1, nums2) => {
 
 console.log(findDifference(nums1 = [1,2,3], nums2 = [2,4,6]))
 console.log(findDifference(nums1 = [1,2,3,3], nums2 = [1,1,2,2]))
-
-
+console.log(findDifference(nums1 = [-68,-80,-19,-94,82,21,-43], nums2 = [-63]))
 
 
 
