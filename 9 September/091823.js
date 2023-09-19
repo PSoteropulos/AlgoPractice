342249a59cc06d8eadc37c5a8ad6fd43eadc8ba2
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-09-18 23:23:19 -0500

    9/18 js solved

--- 9 September/091823.js
+++ 9 September/091823.js
@@ -1,5 +1,11 @@
 const minMovesToSeat = (seats, students) => {
-    
+    let total = 0
+    seats.sort((a,b)=>a-b)
+    students.sort((a,b)=>a-b)
+    for (let i = 0; i<seats.length; i++){
+        total += Math.abs(seats[i]-students[i])
+    }
+    return total
 };
 
 
