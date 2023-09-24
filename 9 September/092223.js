a678fe4dd5510b6bf8e5e9da7dfdea4d7070f968
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-09-24 18:39:19 -0500

    9/22 js solved

--- 9 September/092223.js
+++ 9 September/092223.js
@@ -1,5 +1,11 @@
 const numberOfPoints = (nums) => {
-    
+    let pointSet = new Set()
+    for (let i=0; i<nums.length; i++){
+        for (let j = nums[i][0]; j <= nums[i][1]; j++){
+            pointSet.add(j)
+        }
+    }
+    return pointSet.size
 };
 
 
