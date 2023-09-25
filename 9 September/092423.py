75f3eb55820a6c63ca34c6aa30c902c4f5a61eee
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-09-24 21:51:28 -0500

    9/24 py solved

--- 9 September/092423.py
+++ 9 September/092423.py
@@ -1,5 +1,14 @@
 def makeFancyString(s):
-    pass
+    result = ''
+    count = 1
+    for i in range(len(s)):
+        if (i > 0 and s[i] == s[i - 1]):
+            count += 1
+        else:
+            count = 1;
+        if (count < 3):
+            result += s[i]
+    return result
 
 
 
