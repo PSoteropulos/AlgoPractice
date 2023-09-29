a5af5cb445479b133dee96a029886fc24c282f5c
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-09-28 19:21:46 -0500

    9/28 js solved

--- 9 September/092823.js
+++ 9 September/092823.js
@@ -1,5 +1,20 @@
 const pivotIndex = (nums) => {
-    
+    for (let i = 0; i < nums.length; i++){
+        let leftSum = 0
+        let rightSum = 0
+        for (let j=0; j<nums.length;j++){
+            if (j<i){
+                leftSum += nums[j]
+            }
+            if (j>i){
+                rightSum += nums[j]
+            }
+        }
+        if (leftSum === rightSum){
+            return i
+        }
+    }
+    return -1
 };
 
 
