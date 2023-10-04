95506071f849e403cbb42dd4eff9d530b4e6bb18
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-10-03 19:35:00 -0500

    10/3 reinit

--- 10 October/100323.js
+++ 10 October/100323.js
@@ -1,4 +1,4 @@
-const minimumDifference = (nums, k) => {
+const isCovered = (ranges, left, right) => {
     
 };
 
@@ -8,8 +8,8 @@ const minimumDifference = (nums, k) => {
 
 
 
-console.log(minimumDifference(nums = [90], k = 1))
-console.log(minimumDifference(nums = [9,4,1,7], k = 2))
+console.log(isCovered(ranges = [[1,2],[3,4],[5,6]], left = 2, right = 5))
+console.log(isCovered(ranges = [[1,10],[10,20]], left = 21, right = 21))
 
 
 
@@ -17,47 +17,43 @@ console.log(minimumDifference(nums = [9,4,1,7], k = 2))
 
 
 
-// 1984. Minimum Difference Between Highest and Lowest of K Scores
+
+// 1893. Check if All the Integers in a Range Are Covered
 // Easy
 // Topics
 // Companies
 // Hint
-// You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student. You are also given an integer k.
+// You are given a 2D integer array ranges and two integers left and right. Each ranges[i] = [starti, endi] represents an inclusive interval between starti and endi.
 
-// Pick the scores of any k students from the array so that the difference between the highest and the lowest of the k scores is minimized.
+// Return true if each integer in the inclusive range [left, right] is covered by at least one interval in ranges. Return false otherwise.
 
-// Return the minimum possible difference.
+// An integer x is covered by an interval ranges[i] = [starti, endi] if starti <= x <= endi.
 
  
 
 // Example 1:
 
-// Input: nums = [90], k = 1
-// Output: 0
-// Explanation: There is one way to pick score(s) of one student:
-// - [90]. The difference between the highest and lowest score is 90 - 90 = 0.
-// The minimum possible difference is 0.
+// Input: ranges = [[1,2],[3,4],[5,6]], left = 2, right = 5
+// Output: true
+// Explanation: Every integer between 2 and 5 is covered:
+// - 2 is covered by the first range.
+// - 3 and 4 are covered by the second range.
+// - 5 is covered by the third range.
 // Example 2:
 
-// Input: nums = [9,4,1,7], k = 2
-// Output: 2
-// Explanation: There are six ways to pick score(s) of two students:
-// - [9,4,1,7]. The difference between the highest and lowest score is 9 - 4 = 5.
-// - [9,4,1,7]. The difference between the highest and lowest score is 9 - 1 = 8.
-// - [9,4,1,7]. The difference between the highest and lowest score is 9 - 7 = 2.
-// - [9,4,1,7]. The difference between the highest and lowest score is 4 - 1 = 3.
-// - [9,4,1,7]. The difference between the highest and lowest score is 7 - 4 = 3.
-// - [9,4,1,7]. The difference between the highest and lowest score is 7 - 1 = 6.
-// The minimum possible difference is 2.
+// Input: ranges = [[1,10],[10,20]], left = 21, right = 21
+// Output: false
+// Explanation: 21 is not covered by any range.
  
 
 // Constraints:
 
-// 1 <= k <= nums.length <= 1000
-// 0 <= nums[i] <= 105
+// 1 <= ranges.length <= 50
+// 1 <= starti <= endi <= 50
+// 1 <= left <= right <= 50
 // Accepted
-// 56.8K
+// 35.6K
 // Submissions
-// 102.5K
+// 71.3K
 // Acceptance Rate
-// 55.4%
+// 50.0%
