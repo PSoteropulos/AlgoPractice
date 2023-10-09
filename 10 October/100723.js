ca2544b319e3856d31f15301d32cd1b524f8b3cf
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-10-08 19:09:42 -0500

    10/7 reinit

--- 10 October/100723.js
+++ 10 October/100723.js
@@ -1,4 +1,4 @@
-const garbageCollection = (garbage, travel) => {
+const minOperations = (n) => {
     
 };
 
@@ -7,8 +7,8 @@ const garbageCollection = (garbage, travel) => {
 
 
 
-console.log(garbageCollection(garbage = ["G","P","GP","GG"], travel = [2,4,3]))
-console.log(garbageCollection(garbage = ["MMM","PGM","GP"], travel = [3,10]))
+console.log(minOperations(3))
+console.log(minOperations(6))
 
 
 
@@ -17,65 +17,38 @@ console.log(garbageCollection(garbage = ["MMM","PGM","GP"], travel = [3,10]))
 
 
 
-// 2391. Minimum Amount of Time to Collect Garbage
+// 1551. Minimum Operations to Make Array Equal
 // Medium
 // Topics
 // Companies
 // Hint
-// You are given a 0-indexed array of strings garbage where garbage[i] represents the assortment of garbage at the ith house. garbage[i] consists only of the characters 'M', 'P' and 'G' representing one unit of metal, paper and glass garbage respectively. Picking up one unit of any type of garbage takes 1 minute.
+// You have an array arr of length n where arr[i] = (2 * i) + 1 for all valid values of i (i.e., 0 <= i < n).
 
-// You are also given a 0-indexed integer array travel where travel[i] is the number of minutes needed to go from house i to house i + 1.
+// In one operation, you can select two indices x and y where 0 <= x, y < n and subtract 1 from arr[x] and add 1 to arr[y] (i.e., perform arr[x] -=1 and arr[y] += 1). The goal is to make all the elements of the array equal. It is guaranteed that all the elements of the array can be made equal using some operations.
 
-// There are three garbage trucks in the city, each responsible for picking up one type of garbage. Each garbage truck starts at house 0 and must visit each house in order; however, they do not need to visit every house.
-
-// Only one garbage truck may be used at any given moment. While one truck is driving or picking up garbage, the other two trucks cannot do anything.
-
-// Return the minimum number of minutes needed to pick up all the garbage.
+// Given an integer n, the length of the array, return the minimum number of operations needed to make all the elements of arr equal.
 
  
 
 // Example 1:
 
-// Input: garbage = ["G","P","GP","GG"], travel = [2,4,3]
-// Output: 21
-// Explanation:
-// The paper garbage truck:
-// 1. Travels from house 0 to house 1
-// 2. Collects the paper garbage at house 1
-// 3. Travels from house 1 to house 2
-// 4. Collects the paper garbage at house 2
-// Altogether, it takes 8 minutes to pick up all the paper garbage.
-// The glass garbage truck:
-// 1. Collects the glass garbage at house 0
-// 2. Travels from house 0 to house 1
-// 3. Travels from house 1 to house 2
-// 4. Collects the glass garbage at house 2
-// 5. Travels from house 2 to house 3
-// 6. Collects the glass garbage at house 3
-// Altogether, it takes 13 minutes to pick up all the glass garbage.
-// Since there is no metal garbage, we do not need to consider the metal garbage truck.
-// Therefore, it takes a total of 8 + 13 = 21 minutes to collect all the garbage.
+// Input: n = 3
+// Output: 2
+// Explanation: arr = [1, 3, 5]
+// First operation choose x = 2 and y = 0, this leads arr to be [2, 3, 4]
+// In the second operation choose x = 2 and y = 0 again, thus arr = [3, 3, 3].
 // Example 2:
 
-// Input: garbage = ["MMM","PGM","GP"], travel = [3,10]
-// Output: 37
-// Explanation:
-// The metal garbage truck takes 7 minutes to pick up all the metal garbage.
-// The paper garbage truck takes 15 minutes to pick up all the paper garbage.
-// The glass garbage truck takes 15 minutes to pick up all the glass garbage.
-// It takes a total of 7 + 15 + 15 = 37 minutes to collect all the garbage.
+// Input: n = 6
+// Output: 9
  
 
 // Constraints:
 
-// 2 <= garbage.length <= 105
-// garbage[i] consists of only the letters 'M', 'P', and 'G'.
-// 1 <= garbage[i].length <= 10
-// travel.length == garbage.length - 1
-// 1 <= travel[i] <= 100
+// 1 <= n <= 104
 // Accepted
-// 42K
+// 89K
 // Submissions
-// 50.7K
+// 108.6K
 // Acceptance Rate
-// 82.9%
+// 82.0%
