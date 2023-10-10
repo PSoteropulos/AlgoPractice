da723dfcd536cde3200dcd04fdaebf6f234b0ee6
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-10-09 19:05:27 -0500

    10/9 py solved

--- 10 October/100923.js
+++ 10 October/100923.js
@@ -1,12 +1,10 @@
 const countGoodTriplets = (arr, a, b, c) => {
     let count = 0
-    let answers = []
     for (let i=0; i<arr.length-2;i++){
         for (let j=i+1;j<arr.length-1;j++){
             for (let k=j+1;k<arr.length;k++){
                 if ((Math.abs(arr[i]-arr[j])<=a) && (Math.abs(arr[j]-arr[k])<=b) && (Math.abs(arr[i]-arr[k])<=c)){
                     count ++
-                    answers.push([arr[i], arr[j], arr[k]])
                 }
             }
         }
--- 10 October/100923.py
+++ 10 October/100923.py
@@ -1,5 +1,11 @@
 def countGoodTriplets(arr, a, b, c):
-    pass
+    count = 0
+    for i in range(len(arr)-2):
+        for j in range(i+1, len(arr)-1):
+            for k in range(j+1,len(arr)):
+                if ((abs(arr[i]-arr[j])<=a) and (abs(arr[j]-arr[k])<=b) and (abs(arr[i]-arr[k])<=c)):
+                    count += 1
+    return count
 
 
 
