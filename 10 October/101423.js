195cab033c2b1a3ccbedca5a36d27797408fcca8
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-10-16 01:14:55 -0500

    10/14 js solved

--- 10 October/101423.js
+++ 10 October/101423.js
@@ -1,5 +1,23 @@
 const countGoodRectangles = (rectangles) => {
-    
+    let maxLength = 0
+    let count = 0
+    for (let i=0; i<rectangles.length; i++){
+        let lower = undefined
+        if (rectangles[i][0]<=rectangles[i][1]){
+            lower = rectangles[i][0]
+        }
+        else {
+            lower = rectangles[i][1]
+        }
+        if (lower > maxLength){
+            maxLength = lower
+            count = 1
+        }
+        else if (maxLength == lower){
+            count++
+        }
+    }
+    return count
 };
 
 
