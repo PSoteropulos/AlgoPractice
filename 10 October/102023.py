2daf97ae09e1947c183bcae307a03780f02273ed
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-10-22 01:39:38 -0500

    10/20 py solved

--- 10 October/102023.py
+++ 10 October/102023.py
@@ -1,5 +1,11 @@
 def unequalTriplets(nums):
-    pass
+    count = 0
+    for i in range(len(nums)-2):
+        for j in range(i+1, len(nums)-1):
+            for k in range(j+1, len(nums)):
+                if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
+                    count += 1
+    return count
 
 
 
