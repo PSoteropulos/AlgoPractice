6616e39207331ac851726c0b6a287d2632770ab8
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-10-29 22:12:02 -0500

    10/26 js solved

--- 10 October/102723.js
+++ 10 October/102723.js
@@ -1,5 +1,12 @@
 const findIndices = (nums, indexDifference, valueDifference) => {
-    
+    for (let i=0; i<nums.length-indexDifference;i ++){
+        for (let j=i+indexDifference;j<nums.length; j++){
+            if (Math.abs(nums[i]-nums[j])>=valueDifference){
+                return [i,j]
+            }
+        }
+    }
+    return [-1,-1]
 };
 
 
