278374ffb56474e96d8494a9e5d01700d1435c1e
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-10-29 22:13:15 -0500

    10/26 py solved

--- 10 October/102723.py
+++ 10 October/102723.py
@@ -1,5 +1,9 @@
 def findIndices(nums, indexDifference, valueDifference):
-    pass
+    for i in range(len(nums)-indexDifference):
+        for j in range(i+indexDifference, len(nums)):
+            if (abs(nums[i]-nums[j])>=valueDifference):
+                return [i,j]
+    return [-1,-1]
 
 
 
