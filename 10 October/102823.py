822b05d1d3be8d1cf9a9fbb7949b0f151c0e1ec7
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-10-29 22:26:45 -0500

    10/28 py solved

--- 10 October/102823.py
+++ 10 October/102823.py
@@ -1,5 +1,11 @@
 def minimumSum(nums):
-    pass
+    minSum = 150
+    for i in range(len(nums)-2):
+        for j in range(i+1, len(nums)-1):
+            for k in range(j+1,len(nums)):
+                if ((nums[i] < nums[j]) and (nums[k]<nums[j]) and (nums[i]+nums[j]+nums[k]<minSum)):
+                    minSum = nums[i]+nums[j]+nums[k]
+    return -1 if minSum == 150 else minSum
 
 
 
