410a30ecf93fbd53f2249e2b8d3e29b66a9c0fcf
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-11-19 19:39:05 -0600

    11/18 py solved

--- 11 November/111823.py
+++ 11 November/111823.py
@@ -1,5 +1,17 @@
 def findLengthOfLCIS(nums):
-    pass
+    max = 1
+    current = 1
+    for i in range(1,len(nums)):
+        if (nums[i]>nums[i-1]):
+            current += 1
+        else:
+            if (current>max):
+                max = current
+            current = 1
+        if (i == len(nums)-1):
+            if (current>max):
+                max = current
+    return max
 
 
 
