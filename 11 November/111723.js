9ae415dd3f062f63488d23b4b227cb8aef2c808e
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-11-19 19:29:27 -0600

    11/17 js solved

--- 11 November/111723.js
+++ 11 November/111723.js
@@ -1,5 +1,11 @@
 const countOdds = (low, high) => {
-    
+    let count = 0
+    for (let i = low; i<=high; i++){
+        if (i%2!==0){
+            count ++
+        }
+    }
+    return count
 };
 
 
