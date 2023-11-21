22421cb7282765e28b34e44f422bd3705a1bd335
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-11-21 17:24:14 -0600

    11/21 reinit

---
+++ 11 November/112123.js
@@ -0,0 +1,41 @@
+const convertToBase7 = (num) => {
+    
+};
+
+
+
+
+
+
+
+console.log(convertToBase7(100))
+console.log(convertToBase7(-7))
+
+
+
+
+
+
+
+
+// 504. Base 7
+// Easy
+// Topics
+// Companies
+// Given an integer num, return a string of its base 7 representation.
+
+ 
+
+// Example 1:
+
+// Input: num = 100
+// Output: "202"
+// Example 2:
+
+// Input: num = -7
+// Output: "-10"
+ 
+
+// Constraints:
+
+// -107 <= num <= 107
