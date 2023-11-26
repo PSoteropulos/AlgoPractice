e7c7678d8272bcfc13d786e010d8a8560127f849
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-11-26 16:44:21 -0600

    11/23 js solved

--- 11 November/112323.js
+++ 11 November/112323.js
@@ -1,5 +1,21 @@
 const countBeautifulPairs = (nums) => {
-    
+    const gcd = (num1, num2) => {
+        while (num2 !== 0) {
+            let temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+        return num1;
+    }
+    let count = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (gcd(Number(nums[i].toString()[0]), Number(nums[j].toString().slice(-1))) ===1) {
+                count++;
+            }
+        }
+    }
+    return count;
 };
 
 
