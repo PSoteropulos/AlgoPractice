13c06d0e4caed885abbf2f2177f01b55f9126298
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-12-07 14:45:49 -0600

    12/7 js solved

--- 12 December/120723.js
+++ 12 December/120723.js
@@ -1,5 +1,17 @@
 const strStr = (haystack, needle) => {
-    
+    for (let i = 0; i <haystack.length; i++){
+        if (haystack[i] === needle[0]){
+            let match = true
+            for (let j = 0; j<needle.length; j++){
+                if (haystack[i+j] !== needle[j]){
+                    match = false
+                    break
+                }
+            }
+            if (match) return i
+        }
+    }
+    return -1
 };
 
 
