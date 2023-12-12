11bcbed3e30600aa3d0ee86080a8154dcff24c7b
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-12-11 22:31:53 -0600

    12/11 reinit

--- 12 December/121023.js
+++ 12 December/121023.js
@@ -1,5 +1,7 @@
 const totalMoney = (n) => {
-    
+    let steps = [1, 3, 6, 10, 15, 21, 28]
+    console.log(28*Math.floor(n/7) + ((Math.floor(n/7)-1)*7) + (steps[n%7-1]))
+    console.log(steps[n%7-1])
 };
 
 
@@ -10,7 +12,7 @@ const totalMoney = (n) => {
 console.log(totalMoney(4))
 console.log(totalMoney(10))
 console.log(totalMoney(20))
-
+console.log(totalMoney(28))
 
 
 
@@ -28,7 +30,7 @@ console.log(totalMoney(20))
 // He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
 // Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
 
- 
+
 
 // Example 1:
 
