aefc6a9a648b515309d4f2408e78dc01e3c0424b
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-12-21 19:45:38 -0600

    12/18 py solved and js optimized

--- 12 December/121823.js
+++ 12 December/121823.js
@@ -1,10 +1,10 @@
 const canPlaceFlowers = (flowerbed, n) => {
-    if (flowerbed[0] === 0 && flowerbed.length === 1 && n === 1) return true
+    if (flowerbed[0] === 0 && flowerbed.length === 1 && n <= 1) return true
     if (flowerbed[0] === 0 && flowerbed[1] === 0){
         n--
         flowerbed[0] = 1
     }
-    for (let i = 0; i < flowerbed.length; i++){
+    for (let i = 1; i < flowerbed.length-1; i++){
         if (flowerbed[i-1] === 0 && flowerbed[i] === 0 && flowerbed[i+1] === 0){
             n--
             flowerbed[i] = 1
--- 12 December/121823.py
+++ 12 December/121823.py
@@ -1,5 +1,16 @@
 def canPlaceFlowers(flowerbed, n):
-    pass
+    if flowerbed[0] == 0 and len(flowerbed) == 1 and n <= 1: return True
+    if flowerbed[0] == 0 and flowerbed[1] == 0:
+        n-= 1
+        flowerbed[0] = 1
+    for i in range(1, len(flowerbed)-1):
+        if flowerbed[i-1] == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
+            n-=1
+            flowerbed[i] = 1
+    if flowerbed[-1] == 0 and flowerbed[-2] == 0:
+        n-=1
+        flowerbed[-1] = 1
+    return True if n <= 0 else False
 
 
 
