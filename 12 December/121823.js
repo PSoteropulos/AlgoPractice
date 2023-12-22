55d591763dd35a3689b6f2cd3ec92d46fbfbc177
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-12-21 19:39:59 -0600

    12/18 js solved

--- 12 December/121823.js
+++ 12 December/121823.js
@@ -1,5 +1,20 @@
 const canPlaceFlowers = (flowerbed, n) => {
-    
+    if (flowerbed[0] === 0 && flowerbed.length === 1 && n === 1) return true
+    if (flowerbed[0] === 0 && flowerbed[1] === 0){
+        n--
+        flowerbed[0] = 1
+    }
+    for (let i = 0; i < flowerbed.length; i++){
+        if (flowerbed[i-1] === 0 && flowerbed[i] === 0 && flowerbed[i+1] === 0){
+            n--
+            flowerbed[i] = 1
+        }
+    }
+    if (flowerbed[flowerbed.length-1] === 0 && flowerbed[flowerbed.length-2] === 0){
+        n--
+        flowerbed[flowerbed.length-1] = 1
+    }
+    return n <= 0 ? true : false
 };
 
 
@@ -8,10 +23,10 @@ const canPlaceFlowers = (flowerbed, n) => {
 
 
 
-console.log(canPlaceFlowers(flowerbed = [1,0,0,0,1], n = 1))
-console.log(canPlaceFlowers(flowerbed = [1,0,0,0,1], n = 2))
-
-
+// console.log(canPlaceFlowers(flowerbed = [1,0,0,0,1], n = 1))
+// console.log(canPlaceFlowers(flowerbed = [1,0,0,0,1], n = 2))
+// console.log(canPlaceFlowers(flowerbed = [1,0,0,0,0,1], n = 2))
+console.log(canPlaceFlowers(flowerbed = [0,0,1,0,1], n = 1))
 
 
 
