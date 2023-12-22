d41d2e3b98c173d24aa55b64cad021cb5bed1187
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-12-21 19:08:04 -0600

    12/19 js solved

--- 12 December/121923.js
+++ 12 December/121923.js
@@ -1,5 +1,12 @@
 const missingNumber = (nums) => {
-    
+    nums.sort((a,b)=>a-b)
+    if (nums[0] !== 0) return 0
+    if (nums[nums.length - 1] !== nums.length) return nums.length
+    for (let i = 1; i < nums.length; i++){
+        if (nums[i] !== nums[i-1] + 1){
+            return i
+        }
+    }
 };
 
 
