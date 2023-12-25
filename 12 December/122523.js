836dde7428cff04fff5d90e94dccd982132e8d0f
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-12-25 00:32:24 -0600

    12/25 problem

---
+++ 12 December/122523.js
@@ -0,0 +1,42 @@
+const commonChars = (words) => {
+    
+};
+
+
+
+
+
+
+console.log(commonChars(["bella","label","roller"]))
+console.log(commonChars(["cool","lock","cook"]))
+
+
+
+
+
+
+
+
+// 1002. Find Common Characters
+// Easy
+// Topics
+// Companies
+// Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: words = ["bella","label","roller"]
+// Output: ["e","l","l"]
+// Example 2:
+
+// Input: words = ["cool","lock","cook"]
+// Output: ["c","o"]
+ 
+
+// Constraints:
+
+// 1 <= words.length <= 100
+// 1 <= words[i].length <= 100
+// words[i] consists of lowercase English letters.
