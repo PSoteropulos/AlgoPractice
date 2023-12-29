aab08e12d9cdcdf5856560e40a77f7fc94a37cec
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-12-28 19:42:48 -0600

    12/21 py solved

--- 12 December/122123.py
+++ 12 December/122123.py
@@ -1,5 +1,10 @@
 def plusOne(digits):
-    pass
+    for i in range(len(digits)-1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+    return [1] + digits
 
 
 
