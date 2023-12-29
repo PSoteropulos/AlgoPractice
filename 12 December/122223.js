adeaaf3d8a2ce8514f04a197f53d0097655f7ff7
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-12-28 19:50:19 -0600

    12/22 js solved

--- 12 December/122223.js
+++ 12 December/122223.js
@@ -1,5 +1,10 @@
 const searchInsert = (nums, target) => {
-    
+    for (let i = 0; i< nums.length; i++){
+        if (nums[i]>=target){
+            return i
+        }
+    }
+    return nums.length
 };
 
 
