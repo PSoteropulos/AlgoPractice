5a2ce0815dedc8382ccc5c33d0b200206bcaea15
Paul Soteropulos <complexsimplicity22@yahoo.com>
2024-01-05 20:57:30 -0600

    1/1 js solved

--- 2024/1 January/010124.js
+++ 2024/1 January/010124.js
@@ -1,5 +1,11 @@
 const sortArrayByParity = (nums) => {
-    
+    for (let i = nums.length-1; i>=0; i--){
+        if (nums[i]%2!==0){
+            nums.push(nums[i])
+            nums.splice(i,1)
+        }
+    }
+    return nums
 };
 
 
